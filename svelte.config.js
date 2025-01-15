56deeb24494dd633f507e849d0d5fbd62dbf1a4a
Louis <lp280500@falmouth.ac.uk>
2025-01-15 21:52:57 +0000

    ref-wip:Trying to get github pages to function

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = 'Portfolio-Site';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
