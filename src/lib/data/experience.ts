d6cbba193ad8105786bf3230e268291dd541dd78
Louis <lp280500@falmouth.ac.uk>
2025-01-15 21:14:31 +0000

    ref-wip: Added Experience

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -1,3 +1,5 @@
+import { Link } from '$lib/components/ui/pagination';
+import { href } from '$lib/utils';
 import Assets from './assets';
 import { getSkills } from './skills';
 import { ContractType, type Experience } from './types';
@@ -6,50 +8,50 @@ const title = 'Experience';
 
 const items: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
-		company: 'Self-employed',
+		slug: 'Programming Intern',
+		company: 'Perchang Games',
 		description: 'Creating awesome tools for developers.',
-		contract: ContractType.SelfEmployed,
+		contract: ContractType.Internship,
 		type: 'Software Development',
 		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('ts', 'js'),
-		name: 'Open Source Developer',
-		color: 'red',
-		links: [],
-		logo: Assets.Unknown,
+		period: { from: new Date(2023, 3), to: new Date(2023, 3) },
+		skills: getSkills('CSharp'),
+		name: 'Programming Internship',
+		color: 'orange',
+		links: [{to: "https://perchang.com/index.html", label: "Visit Site", newTab:true}],
+		logo: Assets.PerchangIcon,
 		shortDescription: 'Creating awesome tools for developers.'
 	},
-	{
-		slug: 'software-freelance',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
-		color: 'blue',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
-	},
-	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: 'green',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
-	}
+	// {
+	// 	slug: 'software-freelance',
+	// 	company: 'Self-employed',
+	// 	description: 'Creating awesome applications for customers.',
+	// 	contract: ContractType.Freelance,
+	// 	type: 'Software Development',
+	// 	location: 'Home',
+	// 	period: { from: new Date() },
+	// 	skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
+	// 	name: 'Freelancer',
+	// 	color: 'blue',
+	// 	links: [],
+	// 	logo: Assets.Unknown,
+	// 	shortDescription: 'Creating awesome applications for customers.'
+	// },
+	// {
+	// 	slug: 'software-freelance-junior',
+	// 	company: 'Self-employed',
+	// 	description: 'Creating awesome applications for customers.',
+	// 	contract: ContractType.Freelance,
+	// 	type: 'Software Development',
+	// 	location: 'Home',
+	// 	period: { from: new Date(2022, 0, 1), to: new Date() },
+	// 	skills: getSkills('css', 'html', 'js'),
+	// 	name: 'Junior Freelancer',
+	// 	color: 'green',
+	// 	links: [],
+	// 	logo: Assets.Unknown,
+	// 	shortDescription: 'Creating awesome applications for customers.'
+	// }
 ];
 
 const ExperienceData = { title, items };
