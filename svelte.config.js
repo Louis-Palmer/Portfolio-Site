0b027e1a4bf16e3a898a762cfd49f58dfdf48f52
Louis <lp280500@falmouth.ac.uk>
2025-01-15 21:41:53 +0000

    ref-wip: Tweaked code

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/Portfolio-Site';
+const base = '/portfolio-site';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
