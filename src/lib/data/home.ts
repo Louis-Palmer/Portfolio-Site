6f70f1e1fdc107084f8d92eab1e10ef9cc249b79
Louis <lp280500@falmouth.ac.uk>
2025-01-15 21:16:10 +0000

    ref-fin: Added Details

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -13,9 +13,9 @@ const hero: {
 	description:
 		'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
 	links: [
-		{ label: 'GitHub', href: 'https://github.com', icon: 'i-carbon-logo-github' },
-		{ label: 'LinkedIn', href: 'https://linkedin.com', icon: 'i-carbon-logo-linkedin' },
-		{ label: 'Twitter', href: 'https://twitter.com', icon: 'i-carbon-logo-twitter' },
+		{ label: 'GitHub', href: 'https://github.com/Louis-Palmer', icon: 'i-carbon-logo-github' },
+		{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/louis-palmer-447a1524b/', icon: 'i-carbon-logo-linkedin' },
+		// { label: 'Twitter', href: 'https://twitter.com', icon: 'i-carbon-logo-twitter' },
 		{ label: 'Email', href: 'mailto:riadh.adrani@pm.me', icon: 'i-carbon-at' }
 	]
 };
